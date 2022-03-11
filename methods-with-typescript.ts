9260dcaffa378351f4873ef33ceade43f2f10dca
Ray Flores <rayflores@Rays-MBP.ht.home>
2022-03-10 23:22:21 -0500

    Methods with typescript

---
+++ methods-with-typescript.ts
@@ -0,0 +1,27 @@
+
+
+// methods are functions that belong to classes
+export class Message {
+  title: string;
+  message: string;
+  isSent: boolean;
+
+  constructor(title: string, message: string) {
+    this.title = title;
+    this.message = message;
+    this.isSent = false;
+  }
+
+  //method: create a function that makes a preview (select first 10 characters in message to show users)
+  previewMessage(): string {
+    return this.message.slice(1, 10).concat('...');
+  }
+
+}
+
+const message = new Message(
+  'New Course!',
+  'Check out our latest course on OOP with TypeScript'
+);
+
+message.previewMessage();
