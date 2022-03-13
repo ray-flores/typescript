d8d2c40cbe2467487181cdc6ea62f3070dbe6fb9
Ray Flores <rayflores@Rays-MBP.ht.home>
2022-03-13 06:14:07 -0400

    what to do next

---
+++ whats-next.ts
@@ -0,0 +1,7 @@
+
+// convert an existing JS project using TS syntax
+// got to typescriptlang.org for documentation is official 
+// refactor an existing TS project
+// start a new TS project
+// Learn more details about TS
+
