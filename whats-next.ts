173b1fee5e831f2c8588e988310a4a49a3bfda58
Ray Flores <rayflores@Rays-MBP.ht.home>
2022-03-14 23:51:28 -0400

    Add whats-next.ts

--- whats-next.ts
+++ whats-next.ts
@@ -5,3 +5,22 @@
 // start a new TS project
 // Learn more details about TS
 
+// TS versions: project TS version might be different from VS code TS
+// keep in mind when there's compiling errors
+// look at Typescript version at bottom var in VS code of TS file
+// use undeprecated TSLint version
+
+const message1 = { title: undefined, message: undefined, id: undefined };
+const message2 = { title: undefined, message: undefined, id: undefined };
+const message3 = { title: undefined, message: undefined, id: undefined };
+
+export class Message {
+
+  title;
+  message;
+  id;
+
+}
+
+const message4 = new Message();
+const message5 = new Message();
